61218614f1c6ece9d5b5a7968d21da5789b40ca0
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-11-12 15:18:54 +0500

    Using Stream Builder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,24 +4,52 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-            padding: EdgeInsets.all(8),
-            child: Text("This works"),
-          )
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats/sWlkBqhhWSplcnKeLYZo/messages')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+              return ListTile(
+                title: Text(data['text']),
+              );
+            }).toList(),
+          );
+        },
       ),
+      /*StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/sWlkBqhhWSplcnKeLYZo/messages')
+            .snapshots(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = snapshot.data.documents;
+          return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text("documents[index]['index']"),
+              )
+          );
+
+        }
+      ),*/
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/sWlkBqhhWSplcnKeLYZo/messages')
-              .snapshots()
-              .listen((data) {
-                data.docs.forEach((element) {
-                  print(element['text']);
-                });
-          });
         })
     );
   }
