8a6e1e6ca2a8048d73054c1741648e376d58a760
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-11-13 10:29:23 +0500

    auth screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -26,30 +26,14 @@ class ChatScreen extends StatelessWidget {
           );
         },
       ),
-      /*StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/sWlkBqhhWSplcnKeLYZo/messages')
-            .snapshots(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = snapshot.data.documents;
-          return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text("documents[index]['index']"),
-              )
-          );
-
-        }
-      ),*/
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/sWlkBqhhWSplcnKeLYZo/messages')
+              .add({
+            'text': "Button pressed"
+              });
         })
     );
   }
